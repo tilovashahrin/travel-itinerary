96ff7c3e6573f71416a391345f165b174d5e3bcb
riya9255 <riya.shaik@ontariotechu.net>
2020-11-10 01:45:03 -0500

    login created and linked to homepage

---
+++ lib/Button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutt/auth.service.dart';
+
+import 'main.dart';
+class Button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      margin: EdgeInsets.symmetric(horizontal: 50),
+      decoration: BoxDecoration(
+        color: Colors.cyan[500],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: new RaisedButton(
+            child: new Text(
+                'Login for App)'
+            ),
+            onPressed: () {
+              appAuth.login().then((result) {
+                if (result) {
+                  Navigator.of(context).pushReplacementNamed('/home');
+                }
+              });
+            }
+        ),
+      ),
+    );
+  }
+
+
+}
---
+++ lib/main_screen.dart
@@ -0,0 +1,91 @@
+//tilova  (Riya incorp login to homepage)
+import 'package:flutter/material.dart';
+import 'timeline.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  var _tweets = timeline_dataSource.generatePosts();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text('Home'),//Riya added this to call home when log in is clicked
+      ),
+      body: ListView(
+        children: <Widget>[
+          TweetWidget(
+            tweet: _tweets[0],
+          ),
+          TweetWidget(
+              tweet: _tweets[1]
+          ),
+        ], //ListView
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SizedBox(width: 10.0,
+                height: 10.0),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MainScreen(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.home, size: 40),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    //builder: (context) => _likeScreen(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.favorite, size: 40),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    //builder: (context) => _EventScreen(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.add, size: 40),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    //builder: (context) => _EventScreen(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.person, size: 40),
+            ),
+            SizedBox(
+              height: 10.0,
+              width: 10.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
